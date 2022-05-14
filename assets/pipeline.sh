06308115ffdcd44c79e0e349254639d63a4657a3
Kevin <kh216@students.waikato.ac.nz>
2022-05-14 21:33:04 +1200

    COMPX341-22A-A3 Commiting from CI/CD Pipeline

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,3 +1,10 @@
+echo "(0) Push Update to Github"
+cd ..
+git add .
+git commit -m "COMPX341-22A-A3 Commiting from CI/CD Pipeline"
+git push
+echo "Commit done"
+
 echo "(1) Build (Compile the application)"
 npm install
 npm run build
