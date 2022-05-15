d08e35c6c7e10f4a6812af82f4d6573708bce754
Kevin <kh216@students.waikato.ac.nz>
2022-05-15 19:07:34 +1200

    Updated maintenance and pipeline script

--- assets/Maintenance.sh
+++ assets/Maintenance.sh
@@ -1,8 +1,8 @@
 # Get all ts files and store in in an array
-files = ( $(find . -type f -name "*.ts") )
+files=( $(find . -type f -name "*.ts") )
 
 echo "Start of file edit"
-for i in "$(files[@])" ; do
+for i in "${files[@]}" ; do
     echo "Edit: $i"
     sed -i ' 1 s/.*/&Kevin Han - 1521885/' $i
 done
