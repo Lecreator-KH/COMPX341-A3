51d485e015ef0ca0127b755415d80d0f24159dc8
Kevin <kh216@students.waikato.ac.nz>
2022-05-15 17:40:22 +1200

    COMPX341-22A-A3 fixing compile-time error

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,12 +1,34 @@
-echo "(0) Push Update to Github"
-cd ..
-git add .
-git commit -m "COMPX341-22A-A3 Commiting from CI/CD Pipeline"
-git push
-echo "Commit done"
+if [ -z "$1" ] ; then
+    echo "Please enter a commit message"
+    # Sleep 1 sec so we can see the error message
+    sleep 1s
+    exit
+fi
 
-cd assets
-echo "(1) Build (Compile the application)"
-npm install
-npm run build
-npm run start
+echo "Pipeline start"
+# echo "(0) Push Update to Github"
+# git add .
+# git commit -m "COMPX341-22A-A3 Commiting from CI/CD Pipeline"
+# git push
+# echo "Commit done"
+
+# cd assets
+echo "(1) Install"
+if npm install ; then
+    echo "Successful install"
+
+    echo "(2) Build. See whether we can build the program"
+    if npm run build ; then
+        cd ..
+        git add .
+        git commit -m "$1"
+        git push
+        cd assets
+        echo "(3) Start: Run the application"
+        npm run start
+    else
+        echo "Error: Failed to build, no commit shall be made"
+    fi
+else
+    echo "Error: Unsucessful install"
+fi
