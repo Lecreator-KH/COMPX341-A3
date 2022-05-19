3668417691440f2ff7d1c5968b4edaf25222577c
Kevin <kh216@students.waikato.ac.nz>
2022-05-19 21:42:49 +1200

    Added comments to maintenance script

--- assets/Maintenance.sh
+++ assets/Maintenance.sh
@@ -5,9 +5,14 @@ echo "Start of file edit"
 for i in "${files[@]}" ; do
     sed -i ' 1 s/.*/& Kevin Han - 1521885/' $i
 done
-
+echo "End of file edit"
 # Commit and push to github
+
+echo "Push to GitHub"
 cd ..
 git add .
 git commit -m "COMPX341-22A-Maintenance commit message"
 git push
+echo "Push to GitHub finished"
+# Show the user that the application is done
+sleep 1s
