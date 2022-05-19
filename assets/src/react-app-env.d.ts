f5dc0fca86aae9d2f6e3bf2091e6b7f72026bc84
Kevin <kh216@students.waikato.ac.nz>
2022-05-19 21:43:43 +1200

    COMPX341-22A-Maintenance commit message

--- assets/src/react-app-env.d.ts
+++ assets/src/react-app-env.d.ts
@@ -1,2 +1,2 @@
-// Kevin Han - 1521885
+// Kevin Han - 1521885 Kevin Han - 1521885
 /// <reference types="react-scripts" />
--- assets/src/types/aws-amplify-ui.d.ts
+++ assets/src/types/aws-amplify-ui.d.ts
@@ -1,2 +1,2 @@
-// Kevin Han - 1521885
+// Kevin Han - 1521885 Kevin Han - 1521885
 declare module '@aws-amplify/ui'
